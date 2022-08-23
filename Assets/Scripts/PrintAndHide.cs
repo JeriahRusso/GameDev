55322a01cf884673ef5865910429f135b185bf68
JeriahRusso <jeriah_russo@outlook.com>
2022-08-23 19:15:21 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     private int counter;
+    private int rand;
+
     public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         counter = 3;
+        rand = Random.Range(200, 250);
     }
 
     // Update is called once per frame
@@ -18,5 +21,13 @@ public class PrintAndHide : MonoBehaviour
     {
         counter++;
         Debug.Log(this.gameObject.name + ":" + counter);
+        if(this.gameObject.tag == "Red" && counter == 100)
+        {
+            rend.enabled = false;
+        }
+        else if(this.gameObject.tag == "Blue" && counter == rand)
+        {
+            rend.enabled = false;
+        }
     }
 }
